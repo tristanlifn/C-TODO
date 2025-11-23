c59c0eda92b8204773a106154f85171c77315b18
Tristan Lifshitz-Nielsen <tristanlif@protonmail.com>
2025-11-23 11:54:11 +0100

    added see all tasks; added id's to every task; turned off DOM for csv file

--- main.c
+++ main.c
@@ -2,63 +2,157 @@
 #include <stdlib.h>
 #include <string.h>
 
+// create the taskStruct to hold the task data
 struct taskStruct {
     char name[32];
     char description[256];
 };
 
+// Initialize the functions
 int writeToFile(struct taskStruct task);
-struct taskStruct getTaskInfo();
+struct taskStruct createTask();
+int getTasks();
+char* padStringRight(char *str, int finalLength);
+int getLastId();
 
+// main logic loop
 int main(void) {
+    // Loop the program
     while (1) {
-        printf("Chose a action: \n1. Create a new task\n0. Close program\n");
+        // Initialize variables
         int action;
+        struct taskStruct task;
+        int result;
+
+        // Get what action to preform
+        printf("Chose a action: \n1. Create a new task\n2. Get all tasks\n0. Close program\n");
         scanf("%d", &action);
-        getchar();
+        getchar(); // Remove the \n character from stdin
 
-        if (action == 1) {
-            struct taskStruct task = getTaskInfo();
+        switch (action) {
+            case 1: // Create a new task
+                task = createTask();
 
-            int result = writeToFile(task);
+                result = writeToFile(task); // Write it to the file
 
-            if (result == 1) {
-                printf("\nSuccessfully written to file\n");
-            }
-            else if (result == -1) {
+                if (result == 1) {
+                    printf("\nSuccessfully written to file\n");
+                    break;
+                }
                 printf("\nError writing to file\n");
-            }
-        }
-        else if (action == 0) {
-            printf("Goodbye!\n");
-            return 0;
-        }
-        else {
-            printf("\nNot a valid input, try again\n");
+                break;
+            case 2:
+                result = getTasks();
+
+                if (result == -1) {
+                    printf("\nError reading from file\n");
+                }
+                break;
+                
+            case 0: //Exit the program
+                printf("Goodbye!\n");
+                return 0;
+
+            default: // If the users chosen action is not supported
+                printf("\nNot a valid input, try again\nPress enter to continue\n");
+                scanf("%c");
+                break;
         }
     }
 }
 
+// Write a task to the csv file
 int writeToFile(struct taskStruct task) {
+    // Open the file in append mode
     FILE *fptr = fopen("C:/Users/trist/CLionProjects/untitled/tasks.csv", "a+");
+    if (!fptr) return -1; // Return -1 if the file pointer is null
+
+    const int lastId = getLastId();
+
+    fprintf(fptr, "%d;%s;%s\n", lastId + 1, task.name, task.description); // Write the content of the struct to the file
+
+    fclose(fptr); // Close the file
+    return 1;
+}
+
+// Gets the info for a new task
+struct taskStruct createTask() {
+    // Initialize it
+    struct taskStruct task;
+
+    printf("Task name: \n");
+    fgets(task.name, sizeof(task.name), stdin); // Assigns the input to the name field of the struct
+    task.name[strcspn(task.name, "\n")] = '\0'; // Removes the \n, replaces it with a \0
+
+    printf("\nTask description: \n");
+    fgets(task.description, sizeof(task.description), stdin);
+    task.description[strcspn(task.description, "\n")] = '\0';
+
+    return task; // Return the task struct
+}
+
+int getTasks() {
+    FILE *fptr = fopen("C:/Users/trist/CLionProjects/untitled/tasks.csv", "r");
     if (!fptr) return -1;
 
-    fprintf(fptr, "%s,%s\n", task.name, task.description);
+    int pass = 1;
+    char line[288];
+    char *id;
+    char *name;
+    char *description;
+
+    printf("\n");
+    while (fgets(line, sizeof(line), fptr)) {
+        char *token = strtok(line, ";");
+
+        while (token != NULL) {
+            if (pass == 1) {
+                id = padStringRight(token, 3);
+                pass++;
+            }
+            else if (pass == 2) {
+                name = padStringRight(token, 32);
+                pass++;
+            }
+            else if (pass == 3) {
+                description = token;
+                pass = 1;
+            }
+            token = strtok(NULL, ";");
+        }
+        printf("%s | %s | %s", id, name, description);
+        free(name);
+    }
+    printf("\n\n");
 
     fclose(fptr);
     return 1;
 }
 
-struct taskStruct getTaskInfo() {
-    struct taskStruct task;
+char* padStringRight(char *str, const int finalLength) {
+    char *paddedString = malloc(32 + 1);
+    if (!paddedString) return NULL;
 
-    printf("Task name: \n");
-    fgets(task.name, sizeof(task.name), stdin);
-    task.name[strcspn(task.name, "\n")] = '\0';
+    sprintf(paddedString, "%-*s", finalLength, str);
 
-    printf("Task description: \n");
-    fgets(task.description, sizeof(task.description), stdin);
-    task.description[strcspn(task.description, "\n")] = '\0';
+    return paddedString;
+}
 
-    return task;
+int getLastId() {
+    FILE *file = fopen("C:/Users/trist/CLionProjects/untitled/tasks.csv", "r");
+
+    char line[256];
+    char lastLine[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        strcpy(lastLine, line);
+    }
+
+    fclose(file);
+    if (strtok(lastLine, ";") == "ID") return 0;
+
+    char *token = strtok(lastLine, ";");
+    int lastId = atoi(token);
+
+    return lastId;
 }
